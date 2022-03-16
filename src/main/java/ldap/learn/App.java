758086770ddfc821082e5f97b9328a2162b2f2f5
choniyuazwan <choniyuazwan@gmail.com>
2022-03-16 11:57:13 +0700

    add searching

--- src/main/java/ldap/learn/App.java
+++ src/main/java/ldap/learn/App.java
@@ -1,39 +1,49 @@
 package ldap.learn;
 
-import java.util.Properties;
-
 import javax.naming.AuthenticationException;
 import javax.naming.Context;
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import javax.naming.directory.*;
+import java.util.Properties;
 
 public class App {
 
-	DirContext connection;
-
-	/* create connection during object creation */
-	public void newConnection() {
+	public static void main(String[] args) throws Exception {
 		Properties env = new Properties();
 		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
 		env.put(Context.PROVIDER_URL, "ldap://localhost:389");
 		env.put(Context.SECURITY_PRINCIPAL, "cn=admin,dc=example,dc=com");
 		env.put(Context.SECURITY_CREDENTIALS, "password");
+
+		DirContext connection = null;
+
 		try {
 			connection = new InitialDirContext(env);
-			System.out.println("Hello World! successfully connected to Ldap - " + connection);
+			System.out.println("successfully connected to Ldap - " + connection);
 		} catch (AuthenticationException ex) {
 			System.out.println(ex.getMessage());
 		} catch (NamingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	
-	public static void main(String[] args) throws NamingException {
+		
+		String username = "computingforgeeks";
 
-		 App app = new App();
-		 app.newConnection();
-		  
+//		String searchFilter = "(&(|(objectClass=user)(objectClass=person))(sAMAccountName=${username}))";
+//		String searchFilter = "(&(objectClass=person)(sAMAccountName=" + username + "))";
+		String searchFilter = "(&(objectClass=person)(uid=" + username + "))";
+
+		SearchControls searchControls = new SearchControls();
+		searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+
+		NamingEnumeration<?> results = connection.search("dc=example,dc=com", searchFilter, searchControls);
+		
+		while (results.hasMore()) {
+			SearchResult result = (SearchResult) results.next();
+			Attributes attrs = result.getAttributes();
+			System.out.println(attrs.get("cn"));
+			System.out.println(attrs);
+		}
 	}
 }
